aaad4fd9e6ead20a837133d43aec67c7ebe8d86f
pabrig <rigallipablo@gmail.com>
2020-07-01 20:56:38 -0300

    add testDB

--- functions.js
+++ functions.js
@@ -5,16 +5,61 @@ const age = document.getElementById('age');
 const addBtn = document.getElementById('addBtn');
 const updateBtn = document.getElementById('updateBtn');
 const removeBtn = document.getElementById('removeBtn');
+const refreshBtn = document.getElementById('refreshBtn');
 
 const database = firebase.database();
 const rootRef = database.ref('users');
 
-addBtn.addEventListener('click', (e) => {
-    alert('Botón');
+//ADD
+addBtn.addEventListener('click', (e) => { 
     e.preventDefault();
-    rootRef.child(cuserId.value).set({
+   
+const createId = userId.value;
+const autoId = rootRef.push().key
+rootRef.child(autoId).set({
+    first_name: firstName.value,
+    last_name: lastName.value,
+    age: age.value,
+})  
+});
+
+//UPDATE
+updateBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const newDate = {
         first_name: firstName.value,
         last_name: lastName.value,
-        age: age.value,
-    });
+        age: age.value
+    };
+    const updates = {};
+    updates ['/users/' + userId.value] = newDate;
+    updates ['/super-users/' + userId.value] = newDate;
+    database.ref().update(updates);
+});
+
+//REMOVE
+removeBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    rootRef.child(userId.value).remove()
+    .then(() => {
+        alert(firstName + "user delted from database")
+    })
+    .catch(error => {
+        alert(error)
+    })
+
+});
+
+//REFRESH
+refreshBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    userId.value = "";
+    firstName.value = "";
+    lastName.value = "";
+    age.value = " ";
+   
+})
+
+rootRef.on('value', snapshot => {
+    console.log('An event ocurred atention!!!')
 })
