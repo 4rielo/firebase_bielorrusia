02da7bc8d11514a1e2a5ac77b3e76e517f1c8a9d
4rielo <ascarafia@gmail.com>
2020-07-03 18:55:56 -0300

    Working with read included

--- functions.js
+++ functions.js
@@ -5,16 +5,59 @@ const age = document.getElementById('age');
 const addBtn = document.getElementById('addBtn');
 const updateBtn = document.getElementById('updateBtn');
 const removeBtn = document.getElementById('removeBtn');
+const readBtn = document.getElementById('readBtn');
 
 const database = firebase.database();
 const rootRef = database.ref('users');
 
 addBtn.addEventListener('click', (e) => {
-    alert('Botón');
+    //alert('Botón');
     e.preventDefault();
-    rootRef.child(cuserId.value).set({
+    const autoId = userId.value;//rootRef.push().key;
+    rootRef.child(autoId).set({
         first_name: firstName.value,
         last_name: lastName.value,
         age: age.value,
+    });
+});
+
+
+updateBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const newData = database.ref('users/'+userId.value);
+
+    if(age.value) {
+        newData.child('age').set(age.value);
+    };
+
+    if(firstName.value) {
+        newData.child('first_name').set(firstName.value);
+    };
+
+    if(lastName.value){
+        newData.child('last_name').set(lastName.value);
+    };
+    //rootRef.child(userId.value).update(newData);
+
+});
+
+removeBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(userId.value) {
+        rootRef.child(userId.value).remove()
+        .then( ()=>{
+            window.alert('User removed from database!');
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }
+});
+
+readBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    rootRef.child(userId.value).on('value', snapshot => {
+        console.log(snapshot.val());
     });
 })
