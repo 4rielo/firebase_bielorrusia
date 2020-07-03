e50036bafae9ca9e43ef4733bfc3035db99ca0b9
nico-jsp <66648449+nico-jsp@users.noreply.github.com>
2020-07-03 18:58:56 -0300

    Upload with Read button

--- functions.js
+++ functions.js
@@ -10,11 +10,84 @@ const database = firebase.database();
 const rootRef = database.ref('users');
 
 addBtn.addEventListener('click', (e) => {
-    alert('Botón');
+    //alert('Botón');
     e.preventDefault();
-    rootRef.child(cuserId.value).set({
+    const autoId = rootRef.push().key
+    rootRef.child(autoId).set({
         first_name: firstName.value,
         last_name: lastName.value,
         age: age.value,
     });
-})
+});
+
+
+updateBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    //let age1;
+    //let first_name1;
+    //let last_name1;
+    const updates ={};
+    if (age.value){
+        updates['/users/' + userId.value + '/age'] = age.value;  
+    }
+    
+    if (firstName.value){
+        updates['/users/' + userId.value + '/first_name'] = firstName.value;  
+    }
+    
+    if (lastName.value){
+        updates['/users/' + userId.value + '/last_name'] = lastName.value;  
+    }
+    //updates[remove('/users/' + userId.value + '/firstName')]
+    /*
+    const newData = {
+        age: age1,
+        first_name: first_name1,
+        last_name: last_name1
+    };
+    //in this part him add a new kind of user as 'super-users'... why, i do not know
+    //const updates ={};
+    updates['/users/' + userId.value] = newData;*/
+    //updates['/super-users/'+ userId.value] = newData;
+    database.ref().update(updates);
+});
+
+
+removeBtn.addEventListener('click', e=> {
+   e.preventDefault();
+   rootRef.child(userId.value).remove().then(() => {
+        windows.alert('user removed from database !');
+   })
+   .catch(error => {
+       console.error(error);
+   }); 
+});
+
+readBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    rootRef.child(userId.value).on('value', snapshot => {
+        //console.log(snapshot.val().age);
+        age.value = snapshot.val().age;
+        firstName.value = snapshot.val().first_name;
+        lastName.value = snapshot.val().last_name;
+    });
+});
+
+//rootRef.child(0).on('child_changed', snapshot => {
+//    console.log(snapshot.val());
+//});
+
+//rootRef.orderByKey().limitToLast(2).on('value', snapshot => {
+//    console.log(snapshot.val());
+//});
+
+//rootRef.orderByChlid('age').limitToFirst(2).on('value', snapshot => {
+//    console.log(snapshot.val());
+//});
+
+
+// startAt(), 
+//rootRef.orderByChlid('first_name').equalTo('Ramiro').on('value', snapshot => {
+//    console.log(snapshot.val());
+//});
